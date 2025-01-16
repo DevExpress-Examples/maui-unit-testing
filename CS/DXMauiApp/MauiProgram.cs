bb2b05d8ed4a0ec3440b5d622c4abbb32e6a0882
MariyaGordeeva <mariya.gordeeva@devexpress.com>
2025-01-16 17:32:42 +0400

    net 9.0

--- CS/DXMauiApp/MauiProgram.cs
+++ CS/DXMauiApp/MauiProgram.cs
@@ -1,5 +1,4 @@
 ﻿using DevExpress.Maui;
-using Microsoft.Extensions.Logging;
 
 namespace DXMauiApp;
 
@@ -19,10 +18,6 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
-
-#if DEBUG
-		builder.Logging.AddDebug();
-#endif
 
 		return builder.Build();
 	}
